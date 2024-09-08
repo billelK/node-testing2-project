575ec3c4069e1120acea9c5e02c3f72b97139be7
billelK <bilalkhansal@gmail.com>
2024-09-08 18:58:50 +0200

    wip

---
+++ data/migrations/20240908160715_first-migration.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable("users", tbl => {
+    tbl.increments()
+    tbl.text("name",128)
+        .notNullable()
+        .unique()
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("users")
+};
