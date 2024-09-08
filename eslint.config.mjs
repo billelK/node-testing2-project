fb7d38e9ccc52dc3827bc271cf10780a72d46a38
billelK <bilalkhansal@gmail.com>
2024-09-08 17:59:41 +0200

    wip

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import pluginJs from "@eslint/js";
 
 
 export default [
+  {"node": true},
   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
